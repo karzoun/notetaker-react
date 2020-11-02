0f133eeeae573884082d60a616eda9ee5f0ddc7d
“karzoun” <karzoun_27@hotmail.com>
2020-11-01 22:09:52 -0800

    added post and del

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+// import uuid from "uuid";
+import axios from "axios";
+
 import "./App.css";
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: 1,
-        title: "Take out the trash",
-        completed: false,
-      },
-      {
-        id: 2,
-        title: "Dinner",
-        completed: false,
-      },
-      {
-        id: 3,
-        title: "Metting",
-        completed: false,
-      },
-    ],
+    todos: [],
+
+    componentDidMount() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => this.setState({ todos: res.data }));
+    },
   };
   markComplete = (id) => {
     this.setState({
@@ -32,12 +29,43 @@ class App extends Component {
       }),
     });
   };
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
   render() {
     console.log(this.state.todos);
     return (
-      <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -22,6 +22,9 @@ export class TodoItem extends Component {
             onChange={this.props.markComplete.bind(this, id)}
           />{" "}
           {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
         </p>
       </div>
     );
@@ -30,6 +33,15 @@ export class TodoItem extends Component {
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
+};
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 10px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right",
 };
 
 export default TodoItem;
